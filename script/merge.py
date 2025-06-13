60243671128262a744ca99167da438cef4915d35
Théo <theo@jumbomana.fr>
2025-06-13 23:53:31 +0200

    merge .pt

--- script/merge.py
+++ script/merge.py
@@ -1,17 +1,17 @@
 import torch
+import os
 
-files_templates = ['export_sparse_NONperm_0', 'export_sparse_NONperm_1', 'export_sparse_NONperm_2']
 files = []
-for k in range(4, 38):
-    for file in files_templates:
-        files.append(file + f'_{k}.pt')
+for filename in os.listdir('.'):
+    if filename.endswith('.pt'):
+        files.append(filename)
 
 list_tensor = []
 
 for file in files:
-    list_tensor.append(torch.load(file))
+    tensor = torch.load(file, map_location='cpu')
+    list_tensor.append(tensor.squeeze(1))
 tensor_tot = torch.concat(list_tensor, dim=0)
-
 torch.save(tensor_tot, 'export_tot.pt')
 
 
