6441a7db7e59778676d53bfec44d61ce177fbf21
Théo <theo@jumbomana.fr>
2025-06-14 09:19:23 +0200

    Add multi-gpu support

--- script/generation.py
+++ script/generation.py
@@ -1,30 +1,36 @@
 import argparse
+import concurrent.futures
 
 from tqdm import tqdm
 
 from src.apn.apn import *
 
-
+def make(batch_size, device):
+    while True:
+        F = torch.randint(0, 64, (batch_size, 64), device=device)
+        gradient_descent(F, T, trace_table)
+        list_P.append(F)
+        count += args.batch
+        if len(list_P) % 20 == 0:
+            torch.save(torch.cat(list_P, dim=0), f'training_{device}_{count}.pt')
+            list_P = []
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--device', default='cpu')
-    parser.add_argument('--batch', default=10, type=int)
+    parser.add_argument('--num-device', default=0, type=int)
+    parser.add_argument('--batch-size', default=10, type=int)
+
     args = parser.parse_args()
 
-    T = add_table(6, device='cuda:0')
-    trace_table = compute_trace_table(6, device='cuda:0')
+    T = add_table(6, device=args.device)
+    trace_table = compute_trace_table(6, device=args.device)
 
     list_P = []
     count = 0
-
-    with tqdm() as pbar:
-        while True:
-            F = torch.randint(0, 64, (args.batch, 64), device='cuda:0')
-            gradient_descent(F, T, trace_table)
-            list_P.append(F)
-            count += args.batch
-            pbar.update(args.batch)
-            if len(list_P) % 500 == 0:
-                torch.save(torch.cat(list_P, dim=0), f'training_{args.device}_{count}.pt')
-                list_P = []
+    to_do = [f'cuda:{k}' for k in range(args.num_device)]
+    with concurrent.futures.ProcessPoolExecutor(max_workers=args.max_workers) as executor:
+        futures = []
+        for device in enumerate(to_do):
+            futures.append(executor.submit(make, device, args.batch_size))
+        for _ in tqdm(concurrent.futures.as_completed(futures), desc="Overall progress", position=0, total=len(futures)):
+            pass
